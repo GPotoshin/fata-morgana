b82d2dfd281db531de1dc0a3e625e19f690efe4b
George Potoshin <george.potoshin@gmail.com>
2024-06-01 23:19:02 +0200

    cleaner

--- c-src/fmmath.c
+++
@@ -1,9 +0,0 @@
-#include <math.h>
-
-float vector_angle (int x, int y) {
-    if (y >= 0) {
-        return atan2(y,x);
-    } else {
-        return atan2(y,x)+2*M_PI;
-    }
-}
--- c-src/fmmath.h
+++
@@ -1,6 +0,0 @@
-#ifndef FMMATH_H
-#define FMMATH_H
-
-float vector_angle (int x, int y);
-
-#endif /* FMMATH_H */
