ca51de5720d6587ed7786bde6a03cf929e2e5e05
George Potoshin <george.potoshin@gmail.com>
2024-06-01 23:20:07 +0200

    clean

--- zig-cache/o/b3aa66bea1bf74b06f8b53b42b094df9/cimport.h
+++
@@ -1,4 +0,0 @@
-#include <libavcodec/avcodec.h>
-#include <libavutil/opt.h>
-#include <libavutil/imgutils.h>
-#include <stdio.h>
--- zig-cache/o/feb9dc4e610ce565f3e6b435bbd51344/cimport.h
+++
@@ -1,6 +0,0 @@
-#include <libavcodec/avcodec.h>
-#include <libavutil/opt.h>
-#include <libavutil/imgutils.h>
-#include <stdio.h>
-#include <ft2build.h>
-#include <freetype/freetype.h>
